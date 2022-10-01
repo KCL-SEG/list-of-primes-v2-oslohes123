9df72e57f1579650f49b0a79693bce35cc2f02b5
Ashley Tyagi <ashleytyagi@outlook.com>
2022-10-01 08:36:40 +0100

    Add valueError

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError
+    list = [2]
+    divided = False
+    counter = 3
+    primes_found = 1
+    while primes_found < number_of_primes:
+        for y in range(int(math.sqrt(counter)), counter):
+            if counter % y == 0 and y != 1 and counter != y:
+                divided = True
+                break
+        if not divided:
+            list.append(counter)
+            primes_found += 1
+        divided = False
+        counter += 1
     return list
+
+primes(0)
